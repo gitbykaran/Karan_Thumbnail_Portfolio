0e1d6cfb6a60f73e369f4c4a328cca0caa4c8915
Karan <karandeepsingh3451@gmail.com>
2026-02-05 17:43:19 +0530

    2025 to 2026 changed

--- src/sections/Footer/Footer.jsx
+++ src/sections/Footer/Footer.jsx
@@ -4,7 +4,7 @@ function Footer() {
   return (
     <section id="footer" className={styles.container}>
       <p>
-        &copy; 2025 Karandeep Singh. <br />
+        &copy; 2026 Karandeep Singh. <br />
         All rights reserved.
       </p>
     </section>
